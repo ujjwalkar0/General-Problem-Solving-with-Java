021d6f7400871139990aea25ad623d2f96e6e93d
ujjwa <ujjwa@DESKTOP-UDRUQ39>
2024-05-12 11:36:11 +0530

    Add project files.

---
+++ Problem Solving/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Problem_Solving
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Problem Solving/SubstringProblem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_Solving
+{
+    public partial class Solution
+    {
+        public int isSameString(string haystack, string needle, int j)
+        {
+            if (haystack.Length - j < needle.Length) return -1;
+
+            for (int i = 0; i < needle.Length; i++)
+            {
+                if (haystack[i + j] != needle[i])
+                {
+                    return -1;
+                }
+            }
+            return j;
+        }
+        public int StrStr(string haystack, string needle)
+        {
+            int index = -1;
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    index = isSameString(haystack, needle, i);
+
+                    if (index != -1)
+                        return index;
+                }
+            }
+
+            return index;
+        }
+    }
+}
