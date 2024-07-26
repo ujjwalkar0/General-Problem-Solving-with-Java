69df23a265e3e70869104de1ddff9250034aedcb
Ujjwal Kar <ujjwalkar21@gmail.com>
2024-07-26 22:44:50 +0530

    Longest Palindromic Substring

---
+++ Problem Solving/LongestPalindrome.cs
@@ -0,0 +1,66 @@
+﻿// https://leetcode.com/problems/longest-palindromic-substring/description/
+
+namespace Problem_Solving
+{
+    public partial class Solution
+    {
+
+        public bool isPalindrom(string s)
+        {
+
+            if (s.Length == 1)
+            {
+                return true;
+            }
+
+            for (int i = 0; i <= s.Length/2; i++)
+            {
+                if (s[i] != s[s.Length - i -1])
+                    return false;
+            }
+            return true;
+        }
+
+        public bool isLongestPalindrom(string s, int currentLongestlen)
+        {
+            if (isPalindrom(s))
+            {
+                if (s.Length > currentLongestlen)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public string LongestPalindrome(string s)
+        {
+            string subStr = "";
+            string longestSubstring = "";
+
+            if (s.Length == 1)
+            {
+                return s;
+            }
+
+            if ((s.Length == 2) && (s[0] == s[1]))
+            {
+                return s;
+            }
+
+
+            for (int start = 0; start < s.Length; start++)
+            {
+                for (int size = 1; size <= s.Length-start; size++)
+                {
+                    subStr = s.Substring(start, size);
+                    if (isLongestPalindrom(subStr, longestSubstring.Length))
+                    {
+                        longestSubstring = subStr;
+                    }
+                }
+            }
+            return longestSubstring;
+        }
+    }
+}
---
+++ Test/LongestPalindrome.cs
@@ -0,0 +1,92 @@
+﻿// https://leetcode.com/problems/longest-palindromic-substring/description/
+using Problem_Solving;
+
+namespace Test
+{
+    public class LongestPalindrome
+    {
+        [Fact]
+        public void Test1()
+        {
+            Solution solution = new Solution();
+            string inputStr = "a";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result=="a");
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            Solution solution = new Solution();
+            string inputStr = "aa";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result == "aa");
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            Solution solution = new Solution();
+            string inputStr = "ab";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result == "a" || result == "b");
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            Solution solution = new Solution();
+            string inputStr = "abab";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result == "aba");
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            Solution solution = new Solution();
+            string inputStr = "abb";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result == "bb");
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            Solution solution = new Solution();
+            string inputStr = "abaabaa";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result == "abaaba");
+        }
+
+        [Fact]
+        public void Test7()
+        {
+            Solution solution = new Solution();
+            string inputStr = "abba";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result == "abba");
+        }
+
+        [Fact]
+        public void Test8()
+        {
+            Solution solution = new Solution();
+            string inputStr = "aaaa";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result == "aaaa");
+        }
+
+        [Fact]
+        public void Test9()
+        {
+            Solution solution = new Solution();
+            string inputStr = "aaaaa";
+            string result = solution.LongestPalindrome(inputStr);
+            Assert.True(result == "aaaaa");
+        }
+
+
+
+    }
+}
